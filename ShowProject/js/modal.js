9be18048615f26aa00dd2335eacc525f619e5199
Sherzod Makhmudov <sherkka0204@gmail.com>
2020-05-31 11:35:11 -0400

    Adding image slider

--- ShowProject/js/modal.js
+++
@@ -1,7 +0,0 @@
-var modal = document.getElementById('myModal');
-var moreScreensGearUp = document.getElementById("more-screens-gearUp");
-
-moreScreensGearUp.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = "img/gearUp.png"
-}
